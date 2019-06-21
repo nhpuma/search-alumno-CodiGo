06d81609baad69e80087ca4fd0c71d23e2a574f8
Nielsen Puma <nielhpuma@gmail.com>
2019-06-21 10:38:33 -0500

    Commit de Inicio del Proyecto

---
+++ search-alumno-CodiGo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace search_alumno_CodiGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("VERIFICACIÓN DE ALUMNO");
+        }
+    }
+}
