d98e2fd5e2cb90db80809e3774cdf120dbfd0eab
Nielsen Puma <nielhpuma@gmail.com>
2019-06-21 10:59:32 -0500

    Menú básico de Búsqueda

--- search-alumno-CodiGo/Program.cs
+++ search-alumno-CodiGo/Program.cs
@@ -6,8 +6,15 @@ namespace search_alumno_CodiGo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("VERIFICACIÓN DE ALUMNO");
+            String dni;
+            
+            Console.WriteLine("=== VERIFICACIÓN DE ALUMNO ===");
+            Console.Write("Ingrese el número de DNI: ");
+            dni = Console.ReadLine().ToString();
+
+            // Llamada a la función buscar
         }
+
+
     }
 }
